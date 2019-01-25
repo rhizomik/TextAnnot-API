c1e64f386c36c52d1e00732b15319cc3b419f726
Roberto García <roberto@rhizomik.net>
2019-01-25 15:05:03 +0100

    Fix upload controller not retrieving property the MetadataTemplate

--- src/main/java/cat/udl/eps/entsoftarch/textannot/controller/UploadController.java
+++ src/main/java/cat/udl/eps/entsoftarch/textannot/controller/UploadController.java
@@ -6,19 +6,17 @@ import cat.udl.eps.entsoftarch.textannot.repository.MetadataTemplateRepository;
 import cat.udl.eps.entsoftarch.textannot.repository.XmlSampleRepository;
 import cat.udl.eps.entsoftarch.textannot.service.XMLIngestionService;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
 import java.util.Optional;
-import javax.xml.parsers.ParserConfigurationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.xml.sax.SAXException;
 
 @RequestMapping("/upload")
 @BasePathAwareController
@@ -30,11 +28,11 @@ public class UploadController {
 
   @PostMapping("/xmlsample")
   @ResponseBody
-  public void uploadXML(MultipartHttpServletRequest request)
-          throws Exception {
+  public void uploadXML(MultipartHttpServletRequest request) throws Exception {
     String templateUri = request.getParameter("metadataTemplate");
-    String templateName = templateUri.substring(templateUri.lastIndexOf('/') + 1);
-    Optional<MetadataTemplate> metadataTemplateOptional = metadataTemplateRepository.findByName(templateName);
+    Integer templateId = Integer.parseInt(templateUri.substring(templateUri.lastIndexOf('/') + 1));
+    Optional<MetadataTemplate> template = metadataTemplateRepository.findById(templateId);
+    Assert.isTrue(template.isPresent(), "The specified MetadataTemplate does not exist");
 
     Iterator<String> fileNames = request.getFileNames();
     MultipartFile xmlFile = request.getFile(fileNames.next());
@@ -48,7 +46,7 @@ public class UploadController {
       result.write(buffer, 0, length);
     }
     xmlSample.setContent(result.toString("UTF-8"));
-    xmlSample.setDescribedBy(metadataTemplateOptional.orElseThrow(() -> new Exception("metadataTemplate Optional not found")));
+    xmlSample.setDescribedBy(template.get());
     xmlService.ingest(xmlSample);
   }
 }
