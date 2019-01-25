9f95b92002ea76520bb700cab2388298c6b17cdf
Roberto García <roberto@rhizomik.net>
2019-01-25 17:03:02 +0100

    Change DB table name for Sample to TextAnnotSample

--- src/main/java/cat/udl/eps/entsoftarch/textannot/domain/Sample.java
+++ src/main/java/cat/udl/eps/entsoftarch/textannot/domain/Sample.java
@@ -1,14 +1,13 @@
 package cat.udl.eps.entsoftarch.textannot.domain;
 
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
-import java.util.ArrayList;
-import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.Data;
@@ -16,6 +15,7 @@ import lombok.EqualsAndHashCode;
 
 @Entity
 @Data
+@Table(name = "TextAnnotSample") //Avoid DB reserved word Sample
 @EqualsAndHashCode(callSuper = true)
 public class Sample extends UriEntity<Integer>{
 
