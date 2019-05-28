b8c9e2b34b7bd222c55ae9c583cfa6e7d5d11add
Albert Berga <albertbergagatius@gmail.com>
2019-05-28 10:50:03 +0200

    Metadata statistics

--- src/main/java/cat/udl/eps/entsoftarch/textannot/controller/SampleFilterController.java
+++ src/main/java/cat/udl/eps/entsoftarch/textannot/controller/SampleFilterController.java
@@ -3,7 +3,9 @@ package cat.udl.eps.entsoftarch.textannot.controller;
 import cat.udl.eps.entsoftarch.textannot.domain.*;
 import cat.udl.eps.entsoftarch.textannot.repository.SampleRepository;
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.QTuple;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQuery;
@@ -25,6 +27,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.persistence.EntityManager;
 import java.io.Serializable;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -39,6 +42,17 @@ public class SampleFilterController {
 
     @PostMapping("/samples/filter")
     public @ResponseBody PagedResources<PersistentEntityResource> getFilteredSamples(@RequestBody SampleFilters filters, Pageable pageable, PagedResourcesAssembler resourceAssembler) {
+        BooleanExpression query = getFiltersExpression(filters);
+        Page<Sample> samples = sampleRepository.findAll(query, pageable);
+
+        if (!samples.hasContent()) {
+            return resourceAssembler.toEmptyResource(samples, Sample.class);
+        }
+
+        return resourceAssembler.toResource(samples);
+    }
+
+    private BooleanExpression getFiltersExpression(@RequestBody SampleFilters filters) {
         List<Integer> samplesContainingWord = sampleRepository.findByTextContainingWord(filters.getWord());
         BooleanExpression query = QSample.sample.id.in(samplesContainingWord);
         if(filters.getMetadata() != null && !filters.getMetadata().isEmpty()) {
@@ -54,13 +68,42 @@ public class SampleFilterController {
                         .where(QAnnotation.annotation.sample.id.eq(QSample.sample.id).and(QTag.tag.name.eq(tag))).exists());
             }
         }
-        Page<Sample> samples = sampleRepository.findAll(query, pageable);
+        return query;
+    }
 
-        if (!samples.hasContent()) {
-            return resourceAssembler.toEmptyResource(samples, Sample.class);
+    @PostMapping("/samples/filter/statistics")
+    public @ResponseBody StatisticsResults getFilteredSamplesStatistics(@RequestBody SampleFilters filters) {
+        StatisticsResults statisticsResults = new StatisticsResults();
+        statisticsResults.setMetadataStadistics(getMetadataStatistics(filters));
+
+        return statisticsResults;
+    }
+
+    private Map<String, Map<String, Long>> getMetadataStatistics(@RequestBody SampleFilters filters) {
+        JPAQuery query = new JPAQuery(em);
+        query = (JPAQuery) query.select(QMetadataField.metadataField.name, QMetadataValue.metadataValue.value, QSample.sample.count())
+                .from(QMetadataValue.metadataValue).innerJoin(QMetadataValue.metadataValue.forA, QSample.sample).innerJoin(QMetadataValue.metadataValue.values, QMetadataField.metadataField)
+                .where(getFiltersExpression(filters)).groupBy(QMetadataField.metadataField.name, QMetadataValue.metadataValue.value);
+        List<Tuple> result = query.fetch();
+        Map<String, Map<String, Long>> statistics = new HashMap<>();
+        result.forEach(qTuple -> {
+            if (!statistics.containsKey(qTuple.get(QMetadataField.metadataField.name)))
+                statistics.put(qTuple.get(QMetadataField.metadataField.name), new HashMap<>());
+            statistics.get(qTuple.get(QMetadataField.metadataField.name)).put(qTuple.get(QMetadataValue.metadataValue.value), qTuple.get(2, Long.TYPE));
+        });
+        return statistics;
+    }
+
+    private static class StatisticsResults implements Serializable {
+        private Map<String, Map<String, Long>> metadataStadistics;
+
+        public Map<String, Map<String, Long>> getMetadataStadistics() {
+            return metadataStadistics;
         }
 
-        return resourceAssembler.toResource(samples);
+        public void setMetadataStadistics(Map<String, Map<String, Long>> metadataStadistics) {
+            this.metadataStadistics = metadataStadistics;
+        }
     }
 
     private static class SampleFilters implements Serializable {
