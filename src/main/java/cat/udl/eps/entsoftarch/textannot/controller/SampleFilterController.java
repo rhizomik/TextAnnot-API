730e58c4e624d428ca95fab11acda47bd0eeacd4
Albert Berga <albertbergagatius@gmail.com>
2019-05-28 12:15:08 +0200

    Statistics return ordered

--- src/main/java/cat/udl/eps/entsoftarch/textannot/controller/SampleFilterController.java
+++ src/main/java/cat/udl/eps/entsoftarch/textannot/controller/SampleFilterController.java
@@ -4,6 +4,8 @@ import cat.udl.eps.entsoftarch.textannot.domain.*;
 import cat.udl.eps.entsoftarch.textannot.repository.SampleRepository;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberPath;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQuery;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +22,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.persistence.EntityManager;
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
@@ -38,7 +40,8 @@ public class SampleFilterController {
     EntityManager em;
 
     @PostMapping("/samples/filter")
-    public @ResponseBody PagedResources<PersistentEntityResource> getFilteredSamples(@RequestBody SampleFilters filters, Pageable pageable, PagedResourcesAssembler resourceAssembler) {
+    public @ResponseBody
+    PagedResources<PersistentEntityResource> getFilteredSamples(@RequestBody SampleFilters filters, Pageable pageable, PagedResourcesAssembler resourceAssembler) {
         BooleanExpression query = getFiltersExpression(filters);
         Page<Sample> samples = sampleRepository.findAll(query, pageable);
 
@@ -52,15 +55,15 @@ public class SampleFilterController {
     private BooleanExpression getFiltersExpression(@RequestBody SampleFilters filters) {
         List<Integer> samplesContainingWord = sampleRepository.findByTextContainingWord(filters.getWord());
         BooleanExpression query = QSample.sample.id.in(samplesContainingWord);
-        if(filters.getMetadata() != null && !filters.getMetadata().isEmpty()) {
-            for (Map.Entry<String, String> e: filters.getMetadata().entrySet()) {
-                 query = query.and(QSample.sample.id.in(JPAExpressions.select(QMetadataValue.metadataValue.forA.id).from(QMetadataValue.metadataValue)
-                         .innerJoin(QMetadataValue.metadataValue.values, QMetadataField.metadataField)
-                         .where(QMetadataValue.metadataValue.value.eq(e.getValue()).and(QMetadataField.metadataField.name.eq(e.getKey())))));
+        if (filters.getMetadata() != null && !filters.getMetadata().isEmpty()) {
+            for (Map.Entry<String, String> e : filters.getMetadata().entrySet()) {
+                query = query.and(QSample.sample.id.in(JPAExpressions.select(QMetadataValue.metadataValue.forA.id).from(QMetadataValue.metadataValue)
+                        .innerJoin(QMetadataValue.metadataValue.values, QMetadataField.metadataField)
+                        .where(QMetadataValue.metadataValue.value.eq(e.getValue()).and(QMetadataField.metadataField.name.eq(e.getKey())))));
             }
         }
-        if(filters.getTags() != null && !filters.getTags().isEmpty()) {
-            for (String tag: filters.getTags()) {
+        if (filters.getTags() != null && !filters.getTags().isEmpty()) {
+            for (String tag : filters.getTags()) {
                 query = query.and(JPAExpressions.selectFrom(QAnnotation.annotation).innerJoin(QAnnotation.annotation.tag, QTag.tag)
                         .where(QAnnotation.annotation.sample.id.eq(QSample.sample.id).and(QTag.tag.name.eq(tag))).exists());
             }
@@ -69,7 +72,8 @@ public class SampleFilterController {
     }
 
     @PostMapping("/samples/filter/statistics")
-    public @ResponseBody StatisticsResults getFilteredSamplesStatistics(@RequestBody SampleFilters filters) {
+    public @ResponseBody
+    StatisticsResults getFilteredSamplesStatistics(@RequestBody SampleFilters filters) {
         StatisticsResults statisticsResults = new StatisticsResults();
         statisticsResults.setMetadataStadistics(getMetadataStatistics(filters));
         Pair<Long, Long> counts = getSampleCounts(filters);
@@ -90,7 +94,7 @@ public class SampleFilterController {
     }
 
     private int getTextOccurrences(String word, String text) {
-        Pattern pattern = Pattern.compile("\\b"+word+"\\b");
+        Pattern pattern = Pattern.compile("\\b" + word + "\\b");
         Matcher matcher = pattern.matcher(text);
         int sampleOccurrences = 0;
         while (matcher.find())
@@ -100,23 +104,25 @@ public class SampleFilterController {
 
     private Map<String, Map<String, Long>> getMetadataStatistics(@RequestBody SampleFilters filters) {
         JPAQuery query = new JPAQuery(em);
-        query = (JPAQuery) query.select(QMetadataField.metadataField.name, QMetadataValue.metadataValue.value, QSample.sample.count())
+        NumberPath<Long> aliasCount = Expressions.numberPath(Long.class, "c");
+        query = (JPAQuery) query.select(QMetadataField.metadataField.name, QMetadataValue.metadataValue.value, QSample.sample.count().as(aliasCount))
                 .from(QMetadataValue.metadataValue)
                 .innerJoin(QMetadataValue.metadataValue.forA, QSample.sample)
                 .innerJoin(QMetadataValue.metadataValue.values, QMetadataField.metadataField)
                 .where(getFiltersExpression(filters))
-                .groupBy(QMetadataField.metadataField.name, QMetadataValue.metadataValue.value);
+                .groupBy(QMetadataField.metadataField.name, QMetadataValue.metadataValue.value)
+                .orderBy(aliasCount.desc());
         List<Tuple> result = query.fetch();
         Map<String, Map<String, Long>> statistics = new HashMap<>();
         result.forEach(qTuple -> {
             if (!statistics.containsKey(qTuple.get(QMetadataField.metadataField.name)))
-                statistics.put(qTuple.get(QMetadataField.metadataField.name), new HashMap<>());
+                statistics.put(qTuple.get(QMetadataField.metadataField.name), new LinkedHashMap<>());
             statistics.get(qTuple.get(QMetadataField.metadataField.name)).put(qTuple.get(QMetadataValue.metadataValue.value), qTuple.get(2, Long.TYPE));
         });
         return statistics;
     }
 
-    private static class StatisticsResults implements Serializable {
+    private static class StatisticsResults {
         private long occurrences;
         private long samples;
         private Map<String, Map<String, Long>> metadataStadistics;
@@ -144,6 +150,7 @@ public class SampleFilterController {
         public void setMetadataStadistics(Map<String, Map<String, Long>> metadataStadistics) {
             this.metadataStadistics = metadataStadistics;
         }
+
     }
 
     private static class SampleFilters implements Serializable {
