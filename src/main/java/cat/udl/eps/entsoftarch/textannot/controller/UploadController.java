1f9de3d7582628c67d50d78585bc7caa7b76ef56
bergacat1 <albertbergagatius@gmail.com>
2019-08-07 11:54:24 +0200

    Fix XMLSample upload tests

--- src/main/java/cat/udl/eps/entsoftarch/textannot/controller/UploadController.java
+++ src/main/java/cat/udl/eps/entsoftarch/textannot/controller/UploadController.java
@@ -5,17 +5,25 @@ import cat.udl.eps.entsoftarch.textannot.domain.XmlSample;
 import cat.udl.eps.entsoftarch.textannot.repository.ProjectRepository;
 import cat.udl.eps.entsoftarch.textannot.repository.XmlSampleRepository;
 import cat.udl.eps.entsoftarch.textannot.service.XMLIngestionService;
+
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.net.URI;
 import java.util.Iterator;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
+import org.springframework.data.rest.webmvc.PersistentEntityResource;
+import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
@@ -23,32 +31,36 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 @BasePathAwareController
 public class UploadController {
 
-  @Autowired XmlSampleRepository xmlSampleRepository;
-  @Autowired
-  ProjectRepository projectRepository;
-  @Autowired XMLIngestionService xmlService;
+    @Autowired
+    XmlSampleRepository xmlSampleRepository;
+    @Autowired
+    ProjectRepository projectRepository;
+    @Autowired
+    XMLIngestionService xmlService;
 
-  @PostMapping("/xmlsample")
-  @ResponseBody
-  public void uploadXML(MultipartHttpServletRequest request) throws Exception {
-    String templateUri = request.getParameter("project");
-    Integer templateId = Integer.parseInt(templateUri.substring(templateUri.lastIndexOf('/') + 1));
-    Optional<Project> project = projectRepository.findById(templateId);
-    Assert.isTrue(project.isPresent(), "The specified Project does not exist");
+    @PostMapping(value = "/xmlsample", consumes = "multipart/form-data")
+    public ResponseEntity<PersistentEntityResource> uploadXML(MultipartHttpServletRequest request, PersistentEntityResourceAssembler resourceAssembler) throws Exception {
+        String templateUri = request.getParameter("project");
+        Integer templateId = Integer.parseInt(templateUri.substring(templateUri.lastIndexOf('/') + 1));
+        Optional<Project> project = projectRepository.findById(templateId);
+        Assert.isTrue(project.isPresent(), "The specified Project does not exist");
 
-    Iterator<String> fileNames = request.getFileNames();
-    MultipartFile xmlFile = request.getFile(fileNames.next());
-    XmlSample xmlSample = new XmlSample();
+        Iterator<String> fileNames = request.getFileNames();
+        MultipartFile xmlFile = request.getFile(fileNames.next());
+        XmlSample xmlSample = new XmlSample();
 
-    ByteArrayOutputStream result = new ByteArrayOutputStream();
-    InputStream input = xmlFile.getInputStream();
-    byte[] buffer = new byte[1024];
-    int length;
-    while ((length = input.read(buffer)) != -1) {
-      result.write(buffer, 0, length);
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        InputStream input = xmlFile.getInputStream();
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = input.read(buffer)) != -1) {
+            result.write(buffer, 0, length);
+        }
+        xmlSample.setContent(result.toString("UTF-8"));
+        xmlSample.setProject(project.get());
+        xmlService.ingest(xmlSample);
+
+        return ResponseEntity.created(new URI(xmlSample.getUri()))
+                .body(resourceAssembler.toResource(xmlSample));
     }
-    xmlSample.setContent(result.toString("UTF-8"));
-    xmlSample.setProject(project.get());
-    xmlService.ingest(xmlSample);
-  }
 }
--- src/test/java/cat/udl/eps/entsoftarch/textannot/steps/XmlSampleStepDefs.java
+++ src/test/java/cat/udl/eps/entsoftarch/textannot/steps/XmlSampleStepDefs.java
@@ -3,8 +3,7 @@ package cat.udl.eps.entsoftarch.textannot.steps;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -13,6 +12,9 @@ import cat.udl.eps.entsoftarch.textannot.domain.*;
 import cat.udl.eps.entsoftarch.textannot.repository.*;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.List;
@@ -21,19 +23,25 @@ import org.hamcrest.Matchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 
 public class XmlSampleStepDefs {
 
     private String newResourceUri;
 
-    @Autowired private StepDefs stepDefs;
+    @Autowired
+    private StepDefs stepDefs;
     private MetadataValue metaValue;
     private MetadataField metaField;
 
-    @Autowired private XmlSampleRepository xmlSampleRepository;
-    @Autowired private ProjectRepository projectRepository;
-    @Autowired private MetadataFieldRepository metadataFieldRepository;
-    @Autowired private MetadataValueRepository metadataValueRepository;
+    @Autowired
+    private XmlSampleRepository xmlSampleRepository;
+    @Autowired
+    private ProjectRepository projectRepository;
+    @Autowired
+    private MetadataFieldRepository metadataFieldRepository;
+    @Autowired
+    private MetadataValueRepository metadataValueRepository;
 
 
     @When("^I upload a XmlSample with text \"([^\"]*)\" and content$")
@@ -77,7 +85,7 @@ public class XmlSampleStepDefs {
 
     @And("^It has been created a new metadataValue with value \"([^\"]*)\" for metadataField with name \"([^\"]*)\"$")
     public void itHasBeenCreatedANewMetadatavauleWithValueForMetadatafieldWithName(String value, String name) throws Throwable {
-        metaValue=metadataValueRepository.findByValue(value);
+        metaValue = metadataValueRepository.findByValue(value);
         stepDefs.result = stepDefs.mockMvc.perform(
                 get("/metadataValues/{id}", metaValue.getId())
                         .accept(MediaType.APPLICATION_JSON)
@@ -97,42 +105,44 @@ public class XmlSampleStepDefs {
     @When("^I upload the XML file with filename \"([^\"]*)\" described by \"([^\"]*)\"$")
     public void iUploadTheXMLFileWithFilename(String filename, String template) throws Throwable {
         XmlSample sample = new XmlSample();
-        Resource resource = stepDefs.wac.getResource("classpath:"+filename);
+        Resource resource = stepDefs.wac.getResource("classpath:" + filename);
         byte[] content = Files.readAllBytes(resource.getFile().toPath());
         sample.setContent(new String(content, StandardCharsets.UTF_8));
         Project project = projectRepository.findByName(template);
         sample.setProject(project);
+        InputStream fileInputStream = getClass().getClassLoader().getResourceAsStream(filename);
+        MockMultipartFile file = new MockMultipartFile("upload", filename, "multipart/form-data", fileInputStream);
+
 
         String message = stepDefs.mapper.writeValueAsString(sample);
 
         stepDefs.result = stepDefs.mockMvc.perform(
-            post("/xmlSamples")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(message)
-                .accept(MediaType.APPLICATION_JSON)
-                .with(AuthenticationStepDefs.authenticate()))
-            .andDo(print());
+                multipart("/upload/xmlsample")
+                        .file(file)
+                        .param("project", project.getUri())
+                        .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
         newResourceUri = stepDefs.result.andReturn().getResponse().getHeader("Location");
     }
 
     @And("^It has been created a XmlSample with the following (\\d+) values")
     public void itHasBeenCreatedAXmlSampleWith(int count, List<List<String>> expectedFieldValues)
-        throws Throwable {
+            throws Throwable {
         stepDefs.result = stepDefs.mockMvc.perform(
-            get("/metadataValues/search/findByForA?sample=" + newResourceUri )
-                .accept(MediaType.APPLICATION_JSON)
-                .with(AuthenticationStepDefs.authenticate()))
-            .andDo(print())
-            .andExpect(status().is(200))
-            .andExpect(jsonPath("$._embedded.metadataValues", hasSize(count)));
+                get("/metadataValues/search/findByForA?sample=" + newResourceUri)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print())
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$._embedded.metadataValues", hasSize(count)));
 
         expectedFieldValues.forEach(expectedFieldValue -> {
             try {
                 stepDefs.result.andExpect(jsonPath(
-                    "$._embedded.metadataValues[?(" +
-                        "@.fieldCategory=='"+expectedFieldValue.get(0)+"' && "+
-                        "@.fieldName=='"+expectedFieldValue.get(1)+"')].value",
-                    hasItem(expectedFieldValue.get(2))));
+                        "$._embedded.metadataValues[?(" +
+                                "@.fieldCategory=='" + expectedFieldValue.get(0) + "' && " +
+                                "@.fieldName=='" + expectedFieldValue.get(1) + "')].value",
+                        hasItem(expectedFieldValue.get(2))));
             } catch (Exception e) {
                 e.printStackTrace();
             }
